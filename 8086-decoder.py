174da196b8a90f2bbfb18175aa10d022756e6a56
Sondre <sondre@sondre.io>
2023-10-11 09:53:26 +0100

    Listing 39 matched

--- 8086-decoder.py
+++ 8086-decoder.py
@@ -15,7 +15,7 @@ REG_NAME_MAP = [
 ]
 
 IMM_TO_REG_MASK = 0b1011 << 4
-MEMREG_TO_REG_MASK = 0b100010 << 2
+MEMREG_TO_MEMREG_MASK = 0b100010 << 2
 IMM_TO_REGMEM_MASK = 0b1100011 << 1
 
 def Decode(FileName : str):
@@ -47,24 +47,108 @@ def Decode(FileName : str):
 
                 Data |= (Byte2 << 8)
             OutLines.append(f"mov {DestReg}, {Data}\n")
-        elif Byte0 & MEMREG_TO_REG_MASK == MEMREG_TO_REG_MASK:
+        elif Byte0 & MEMREG_TO_MEMREG_MASK == MEMREG_TO_MEMREG_MASK:
             # Memory/register to register
             Byte1 = Bytes[ByteIndex]
             ByteIndex += 1
 
+            Mod = Byte1 >> 6
             DBit = (Byte0 & (1 << 1)) >> 1
             WideBit = Byte0 & 1
             RegField = (Byte1 & ((1 << 5) | (1 << 4) | (1 << 3))) >> 3
             RMField = Byte1 & ((1 << 2) | (1 << 1) | 1)
             
-            if DBit:
-                SrcReg = REG_NAME_MAP[RMField][WideBit]
-                DestReg = REG_NAME_MAP[RegField][WideBit]
-            else:
-                SrcReg = REG_NAME_MAP[RegField][WideBit]
-                DestReg = REG_NAME_MAP[RMField][WideBit]
+            if Mod != 0b11:
+                OffsetReg = ""
+                if Mod == 0 and RMField == 0b110:
+                    # TODO: 16-bit direct address
+                    continue
 
-            OutLines.append(f"mov {DestReg}, {SrcReg}\n")
+                if RMField >> 2:
+                    # No offset register
+                    if RMField == 0b100:
+                        AddressReg = "si"
+                    elif RMField == 0b101:
+                        AddressReg = "di"
+                    elif RMField == 0b111:
+                        AddressReg = "bx"
+                else:
+                    if RMField & 1:
+                        OffsetReg = "di"
+                    else:
+                        OffsetReg = "si"
+                    if (RMField >> 1) & 1:
+                        AddressReg = "bp"
+                    else:
+                        AddressReg = "bx"
+
+                if Mod == 0:
+                    # No displacement
+                    if DBit:
+                        DestReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg} + {OffsetReg}]\n")
+                        else:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg}]\n")
+                    else:
+                        SrcReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov [{AddressReg} + {OffsetReg}], {SrcReg}\n")
+                        else:
+                            OutLines.append(f"mov [{AddressReg}], {SrcReg}\n")
+                elif Mod == 0b01:
+                    # 8-bit displacement
+                    Byte2 = Bytes[ByteIndex]
+                    ByteIndex += 1
+
+                    DispLow = Byte2
+                    if DBit:
+                        DestReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg} + {OffsetReg} + {DispLow}]\n")
+                        else:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg} + {DispLow}]\n")
+                    else:
+                        SrcReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov [{AddressReg} + {OffsetReg} + {DispLow}], {SrcReg}\n")
+                        else:
+                            OutLines.append(f"mov [{AddressReg} + {DispLow}], {SrcReg}\n")
+                elif Mod == 0b10:
+                    # 16-bit displacement
+                    Byte2 = Bytes[ByteIndex]
+                    ByteIndex += 1
+                    Byte3 = Bytes[ByteIndex]
+                    ByteIndex += 1
+
+                    DispLow = Byte2
+                    DispHigh = Byte3
+
+                    Disp = (DispHigh << 8) | DispLow
+                    if DBit:
+                        DestReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg} + {OffsetReg} + {Disp}]\n")
+                        else:
+                            OutLines.append(f"mov {DestReg}, [{AddressReg} + {Disp}]\n")
+                    else:
+                        SrcReg = REG_NAME_MAP[RegField][WideBit]
+                        if OffsetReg:
+                            OutLines.append(f"mov [{AddressReg} + {OffsetReg} + {Disp}], {SrcReg}\n")
+                        else:
+                            OutLines.append(f"mov [{AddressReg} + {Disp}], {SrcReg}\n")
+                    
+            else:
+                # No displacement (reg-to-reg)
+                if DBit:
+                    SrcReg = REG_NAME_MAP[RMField][WideBit]
+                    DestReg = REG_NAME_MAP[RegField][WideBit]
+                else:
+                    SrcReg = REG_NAME_MAP[RegField][WideBit]
+                    DestReg = REG_NAME_MAP[RMField][WideBit]
+                OutLines.append(f"mov {DestReg}, {SrcReg}\n")
+
+
         elif Byte0 & IMM_TO_REGMEM_MASK == IMM_TO_REGMEM_MASK:
             # Immediate to register/memory
             pass
--- hw2/babytest.asm
+++ hw2/babytest.asm
@@ -29,3 +29,11 @@ mov cx, 12
 mov cx, -12
 mov dx, 3948
 mov dx, -3948
+
+; Source address calculation
+mov al, [bx + si]
+mov bx, [bp + di]
+mov dx, [bp]
+
+; Source address calculation plus 8-bit displacement
+mov ah, [bx + si + 4]
